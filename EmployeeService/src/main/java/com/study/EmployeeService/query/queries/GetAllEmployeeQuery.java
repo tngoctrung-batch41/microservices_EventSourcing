70dba2e97fd0265b95f80869e6fd055263136f6b
tngoctrung-batch41 <tngoctrung-batch41@sdc.edu.vn>
2023-08-22 11:27:18 +0700

    transactional with SAGA

--- EmployeeService/src/main/java/com/study/EmployeeService/query/queries/GetAllEmployeeQuery.java
+++ EmployeeService/src/main/java/com/study/EmployeeService/query/queries/GetAllEmployeeQuery.java
@@ -1,11 +1,6 @@
 package com.study.EmployeeService.query.queries;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
+
 public class GetAllEmployeeQuery {
 }
